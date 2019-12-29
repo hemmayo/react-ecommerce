98c4e187e5bcfe8ba9207533e798b9438a621673
Emmanuel Popoola <olaayo10@gmail.com>
2019-12-29 20:07:04 +0100

    uadded header component and links to home page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import * as Routes from "./constants/routes";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path={Routes.HomePage} component={HomePage} />
         <Route path={Routes.ShopPage} component={ShopPage} />
